c4cd576a6a6bf5677eb349f33c36ae78682017ac
Adilet <adilet.jenishov@mail.ru>
2025-10-10 12:44:03 +0600

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,36 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders  ({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+
+    const svgLoader = {
+
+        test: /\.svg$/,
+            use: ['@svgr/webpack'],
+
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use:{
+            loader:"babel-loader",
+            options:{
+                presets:['@babel/preset-env'],
+                "plugins":[
+                    [
+                        "i18next-extract",
+                        {
+                            lacales:["ru","en"],
+                            keyAsDefaultValue:true,
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
+
     const cssLoaders =  {
             test: /\.s[ac]ss$/i,
             use: [
@@ -26,7 +56,20 @@ export function buildLoaders  ({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: 'ts-loader',
         exclude: /node_modules/,
     }
+
+    const fileLoaders =  {
+            test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                },
+            ],
+        }
+
     return [
+        fileLoaders,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoaders
     ]
---
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './NavBar.module.scss'
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+
+interface NavBarProps {
+    className?: string;
+}
+
+const NavBar = ({className}: NavBarProps) => {
+    return (
+        <div className={classNames(cls.Navbar,{},[className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Главная </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}> О сайте</AppLink>
+            </div>
+
+        </div>
+    );
+};
+
+export default NavBar;
+
+
+
