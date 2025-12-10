657b604d7a5141767af3f08070c418ee0eb8cd7e
Adilet <adilet.jenishov@mail.ru>
2025-12-10 13:08:45 +0600

    add articles page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BagButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
 import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
